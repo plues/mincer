838a14c6b8cfb0bac0744fe33f993ad2c8c5e290
David Schneider <david.schneider@bivab.de>
2015-09-30 13:50:29 +0200

    module data is now represented as a map from pordnr to module data. So process returns a map  with keys :units and :modules

--- src/mincer/xml/modules.clj
+++ src/mincer/xml/modules.clj
@@ -38,56 +38,48 @@
      :refs     refs}))
 
 (defmethod tree-to-unit-map :default [node]
-  (throw  (IllegalArgumentException. (:tag node))))
+  (throw  (IllegalArgumentException. (name  (:tag node))))) 
 
 (defmulti tree-to-module-map (fn [x & args] (:tag x)))
 
 (defmethod tree-to-module-map :abstract-unit [node]
-  (println "Tag " (:tag node))
   (let [attrs  (:attrs node)]
     {:id (:id attrs)
      :title (:title attrs)
      :type (keyword (:type attrs))
      :semester (extract-semesters (:semester attrs))}))
 
-(defmethod tree-to-module-map :module [node]
-  (println "Tag " (:tag node))
+(defmethod tree-to-module-map :module [node course]
   (let [attrs  (:attrs node)
         pordnr (:pordnr attrs)]
     ; use pordnr as key once it is available
-    (when (not (nil? pordnr))
-      {pordnr {:title (:title attrs)
-                        :abstract-units
+    (when-not (nil? pordnr)
+      {pordnr {:title  (:title attrs)
+               :course course
+               :pordnr pordnr
+                       :abstract-units
                           (map tree-to-module-map (:content node))}})))
 
-(defmethod tree-to-module-map :modules [node]
-  (println "Tag " (:tag node))
-  (let [modules (map tree-to-module-map (:content node))
+(defmethod tree-to-module-map :modules [node course]
+  (let [modules (map tree-to-module-map (:content node) (repeat course))
         filtered-modules (remove nil? modules)]
-    (println "modules" modules)
-    (println "filtered" filtered-modules)
-    (if (not= 0 (count filtered-modules))
-      (apply merge filtered-modules)
-      [])))
+    (when-not (= 0 (count filtered-modules))
+      (apply merge filtered-modules))))
 
 (defmethod tree-to-module-map :course [node]
-  (println "Tag " (:tag node))
-  (println "Content" (:content node))
   ; add course info to modules
-  (let [modules (tree-to-module-map
-                  (first (:content node))) ; there is only one child in course-tag
-        attrs (:attrs node)]
-    {:type :course
-     :id (:id attrs)
-     :title (:title attrs)
-     :modules modules}))
+  ; NOTE: keeping only those courses that have modules (with a pordnr)
+  (let [attrs   (:attrs node)
+        course  {:type  :course
+                 :id    (:id attrs)
+                 :title (:title attrs)}
+        modules (first (:content node))] ; there is only one child in course-tag
+    (tree-to-module-map modules course)))
 
 (defmethod tree-to-module-map :course-module-units [node]
-  (println "Tag " (:tag node))
-  {:cmu (map tree-to-module-map (:content node))})
+  {:modules (apply merge (map tree-to-module-map (:content node)))})
 
-(defmethod tree-to-module-map :units [node] 
-  (println "Tag " (:tag node))
+(defmethod tree-to-module-map :units [node]
   {:units (map tree-to-unit-map (:content node))})
 
 (defmethod tree-to-module-map :default [node]
