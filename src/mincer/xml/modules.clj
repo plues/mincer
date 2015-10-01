d2a54caba8aed1a56d4b1e1fbd02b8dc7a00872d
Jens Bendisposto <jens@bendisposto.de>
2015-10-01 12:47:50 +0200

    minor changes (mainly introducing destructuring)

--- src/mincer/xml/modules.clj
+++ src/mincer/xml/modules.clj
@@ -1,92 +1,84 @@
 (ns mincer.xml.modules
   (:gen-class)
   (:require
-    [mincer.xml.util :refer [get-xml]]))
+   [mincer.xml.util :refer [get-xml]]))
 
 (defn extract-semesters [s]
   (map #(Integer/parseInt %) (clojure.string/split s #",")))
 
-(defmulti tree-to-unit-map (fn [x & args] (:tag x)))
+(defmulti tree-to-unit-map (comp :tag first))
 
-(defmethod tree-to-unit-map :session [node]
-  (let [attrs (:attrs node)]
-    {:type     :session
-     :day      (:day attrs)
-     :time     (Integer/parseInt (:time attrs))
-     :duration (Integer/parseInt (:duration attrs))
-     :rhythm   (Integer/parseInt(:rhythm attrs))}))
+(defmethod tree-to-unit-map :session [{{:keys [day time duration rhythm]} :attrs}]
+  {:type     :session
+   :day      day
+   :time     (Integer/parseInt time)
+   :duration (Integer/parseInt duration)
+   :rhythm   (Integer/parseInt rhythm)})
 
-(defmethod tree-to-unit-map :group [node]
+(defmethod tree-to-unit-map :group [{content :content}]
   {:type     :group
-   :sessions (map tree-to-unit-map (:content node))})
+   :sessions (map tree-to-unit-map content)})
 
-(defmethod tree-to-unit-map :abstract-unit [node]
+(defmethod tree-to-unit-map :abstract-unit [{{id :id} :attrs}]
   {:type :abstract-unit-ref
-   :id   (:id (:attrs node))})
+   :id   id})
 
-(defmethod tree-to-unit-map :unit [node]
-  (let [attrs (:attrs node)
-        children (map tree-to-unit-map (:content node))
+(defmethod tree-to-unit-map :unit [{{:keys [id title semester]} :attrs content :content}]
+  (let [children (map tree-to-unit-map content)
         group-filter (fn [x] (= :group (:type x)))
-        groups (filter group-filter children)
-        refs (filter (complement group-filter) children)]
+        {groups true refs false} (group-by group-filter children)]
     {:type     :unit
-     :id       (:id attrs)
-     :title    (:title attrs)
-     :semester (extract-semesters (:semester attrs))
+     :id       id
+     :title    title
+     :semester (extract-semesters semester)
      :groups   groups
      :refs     refs}))
 
-(defmethod tree-to-unit-map :default [node]
-  (throw  (IllegalArgumentException. (name  (:tag node))))) 
+(defmethod tree-to-unit-map :default [{tag :tag}]
+  (throw  (IllegalArgumentException. (name tag))))
 
-(defmulti tree-to-module-map (fn [x & args] (:tag x)))
+(defmulti tree-to-module-map (comp :tag first))
 
-(defmethod tree-to-module-map :abstract-unit [node]
-  (let [attrs  (:attrs node)]
-    {:id (:id attrs)
-     :title (:title attrs)
-     :type (keyword (:type attrs))
-     :semester (extract-semesters (:semester attrs))}))
+(defmethod tree-to-module-map :abstract-unit [{{:keys [id title type semester]} :attrs}]
+  {:id id
+   :title title
+   :type (keyword type)
+   :semester (extract-semesters semester)})
 
-(defmethod tree-to-module-map :module [node course]
-  (let [attrs  (:attrs node)
-        pordnr (:pordnr attrs)]
-    ; use pordnr as key once it is available
-    (when-not (nil? pordnr)
-      {pordnr {:title  (:title attrs)
-               :course course
-               :pordnr pordnr
-                       :abstract-units
-                          (map tree-to-module-map (:content node))}})))
+(defmethod tree-to-module-map :module [{{:keys [pordnr title]} :attrs content :content} course]
+  (when pordnr 
+    {pordnr {:title  (:title attrs)
+             :course course
+             :pordnr pordnr
+             :abstract-units
+             (map tree-to-module-map content)}}))
 
-(defmethod tree-to-module-map :modules [node course]
-  (let [modules (map tree-to-module-map (:content node) (repeat course))
-        filtered-modules (remove nil? modules)]
-    (when-not (= 0 (count filtered-modules))
+(defmethod tree-to-module-map :modules [{content :content} course]
+  (let [modules (map tree-to-module-map content (repeat course))
+        filtered-modules (filter identity modules)]
+    (when-not (empty? filtered-modules)
       (apply merge filtered-modules))))
 
-(defmethod tree-to-module-map :course [node]
-  ; add course info to modules
-  ; NOTE: keeping only those courses that have modules (with a pordnr)
-  (let [attrs   (:attrs node)
-        course  {:type  :course
-                 :id    (:id attrs)
-                 :title (:title attrs)}
-        modules (first (:content node))] ; there is only one child in course-tag
+(defmethod tree-to-module-map :course [{{:keys [id title]} :attrs content :content}]
+                                        ; add course info to modules
+                                        ; NOTE: keeping only those courses that have modules (with a pordnr)
+  (let [course  {:type  :course
+                 :id   id
+                 :title title}
+        modules (first content)] ; there is only one child in course-tag
     (tree-to-module-map modules course)))
 
-(defmethod tree-to-module-map :course-module-units [node]
-  {:modules (apply merge (map tree-to-module-map (:content node)))})
+(defmethod tree-to-module-map :course-module-units [{:keys [content]}]
+  {:modules (apply merge (map tree-to-module-map content))})
 
-(defmethod tree-to-module-map :units [node]
-  {:units (map tree-to-unit-map (:content node))})
+(defmethod tree-to-module-map :units [{:keys [content]}]
+  {:units (map tree-to-unit-map content)})
 
-(defmethod tree-to-module-map :default [node]
-  (throw  (IllegalArgumentException. (name  (:tag node)))))
+(defmethod tree-to-module-map :default [{:keys [tag]}]
+  (throw  (IllegalArgumentException. (name tag))))
 
-(defn transform [node]
-  (apply merge (map tree-to-module-map (:content node))))
+(defn transform [{:keys [content]}]
+  (apply merge (map tree-to-module-map content)))
 
 (defn process [f]
   (transform (get-xml f)))
