411da61b3aeea839db5b199d2c41a580a999b8ef
David Schneider <david.schneider@bivab.de>
2016-01-18 16:27:21 +0100

    Extracted helpers into mincer.xml.util

--- src/mincer/xml/modules/validator.clj
+++ src/mincer/xml/modules/validator.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require
     [clojure.tools.logging :as log]
-    [clojure.set :refer [difference]]))
+    [mincer.xml.util :refer [check-node check-tag check-attributes]]))
 
 (def required-attributes {:abstract-unit     #{:id :title :type :semester}
                           :abstract-unit-ref #{:id}
@@ -12,46 +12,20 @@
                           :unit              #{:id :title :semester}})
 
 
-(defn- no-content [tag] (into {} (filter (fn [[k v]] (not (= :content k))) tag)))
-(defn- attr-names [tag] (-> tag :attrs keys set))
+(def validate-abstract-unit       (check-node :abstract-unit                    (:abstract-unit required-attributes) nil))
+(def validate-module              (check-node :module                           (:module required-attributes) validate-abstract-unit))
+(def validate-modules             (check-node :modules                          (:modules required-attributes) validate-module))
+(def validate-course              (check-node :course                           (:course required-attributes) validate-modules))
+(def validate-course-module-units (check-node :course-module-units              (:course-module-units required-attributes) validate-course))
 
-
-(defn- check-tag [exp tag]
-  (if-not (nil? (:tag tag))
-    (assert (= exp (:tag tag)) (str "Expected " exp " tag, but was " (no-content tag)))
-    (throw (AssertionError. (str "Expected " exp " tag, but was non-tag " tag)))))
-
-
-(defn- check-attributes [name tag]
-  (let [attrs (attr-names tag)
-        missing (difference (get required-attributes name) attrs)]
-    (assert (= 0 (count missing))
-            (str name " tag " tag " missing required attributes " missing))))
-
-
-(defn check-node
-  ([node-name next-fn] (check-node node-name node-name next-fn))
-  ([node-name attr-name next-fn] (fn [elem]
-                                   (check-tag node-name elem)
-                                   (check-attributes attr-name elem)
-                                   (when-not (nil? next-fn)
-                                     (doall (map next-fn (:content elem)))))))
-
-
-(def validate-abstract-unit (check-node :abstract-unit nil))
-(def validate-module (check-node :module validate-abstract-unit))
-(def validate-modules (check-node :modules validate-module))
-(def validate-course (check-node :course validate-modules))
-(def validate-course-module-units (check-node :course-module-units validate-course))
-
-(def validate-session (check-node :session nil))
-(def validate-group (check-node :group validate-session))
-(def validate-abstract-unit-ref (check-node :abstract-unit :abstract-unit-ref nil))
+(def validate-session             (check-node :session                          (:session required-attributes) nil))
+(def validate-group               (check-node :group                            (:group required-attributes) validate-session))
+(def validate-abstract-unit-ref   (check-node :abstract-unit :abstract-unit-ref (:abstract-unit-ref required-attributes) nil))
 
 
 (defn validate-unit [unit]
   (check-tag :unit unit)
-  (check-attributes :unit unit)
+  (check-attributes :unit unit (:unit required-attributes))
   (doall (map (fn [x]
          (let [tag (:tag x)]
            (cond
@@ -60,7 +34,7 @@
                  :else (assert false (str "Unexpected element " x))))) (:content unit))))
 
 
-(def validate-units (check-node :units validate-unit))
+(def validate-units (check-node :units (:units required-attributes) validate-unit))
 
 
 (defn validate [doc]
--- src/mincer/xml/util.clj
+++ src/mincer/xml/util.clj
@@ -1,6 +1,7 @@
 (ns mincer.xml.util
   (:gen-class)
   (:require
+    [clojure.set :refer [difference]]
     [clojure.xml :as xml]))
 
 (defn get-xml [filename]
@@ -8,3 +9,31 @@
     (java.io.ByteArrayInputStream.
       (.getBytes
         (slurp filename)))))
+
+
+(defn- no-content [tag] (into {} (filter (fn [[k v]] (not (= :content k))) tag)))
+(defn- attr-names [tag] (-> tag :attrs keys set))
+
+
+(defn check-tag [exp tag]
+  (if-not (nil? (:tag tag))
+    (assert (= exp (:tag tag)) (str "Expected " exp " tag, but was " (no-content tag)))
+    (throw (AssertionError. (str "Expected " exp " tag, but was non-tag " tag)))))
+
+
+(defn check-attributes [name tag attr-set]
+  (let [attrs (attr-names tag)
+        missing (difference attr-set attrs)]
+    (assert (= 0 (count missing))
+            (str name " tag " tag " missing required attributes " missing))))
+
+
+(defn check-node
+  ([node-name attr-set next-fn] (check-node node-name node-name attr-set next-fn))
+  ([node-name attr-name attr-set next-fn] (fn [elem]
+                                   (check-tag node-name elem)
+                                   (check-attributes attr-name elem attr-set)
+                                   (when-not (nil? next-fn)
+                                     (doall (map next-fn (:content elem)))))))
+
+
