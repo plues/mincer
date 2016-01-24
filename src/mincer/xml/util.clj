ecd8ec08fa49cf1a8ec1791d6355ccd2bbcf3e40
David Schneider <david.schneider@bivab.de>
2016-01-24 21:58:06 +0100

    Added XSD schema based validation for XML input

--- src/mincer/xml/util.clj
+++ src/mincer/xml/util.clj
@@ -1,10 +1,20 @@
 (ns mincer.xml.util
   (:gen-class)
   (:require
-    [clojure.xml :as xml]))
+    [clojure.xml :as xml])
+  (:import
+    (javax.xml.transform.stream StreamSource)
+    (javax.xml.validation SchemaFactory)
+    (javax.xml XMLConstants)))
 
 (defn get-xml [filename]
   (xml/parse
     (java.io.ByteArrayInputStream.
       (.getBytes
         (slurp filename)))))
+
+(defn validate [schema-file xml-file]
+  (let [sf (SchemaFactory/newInstance XMLConstants/W3C_XML_SCHEMA_NS_URI)
+        schema (.newSchema sf schema-file)
+        validator (.newValidator schema)]
+    (.validate validator (StreamSource. xml-file))))
