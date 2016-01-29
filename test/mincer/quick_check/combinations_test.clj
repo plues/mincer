ba8d517efbd8a77fb817132fa75e51cebae2890d
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-01-29 16:34:17 +0100

    Four different test to confirm selected modules using level tree

--- test/mincer/quick_check/combinations_test.clj
+++ test/mincer/quick_check/combinations_test.clj
@@ -4,44 +4,164 @@
     [clojure.test.check :as tc]
     [clojure.test.check.generators :as gen]
     [clojure.test.check.properties :as prop]
-    [mincer.module-combinations :refer :all]))
+    [mincer.module-combinations :refer :all]
+    [mincer.xml.tree :refer :all]))
 
-(def test_tree
-  [{
-    :required [3]
-    :children
-      [{
-        :required [1 2 3]
-        :children [{:id 1 :children []} {:id 2 :children []} {:id 3 :children []}]}
-      {
-        :required [1 2]
-        :children [{:id 4 :children []} {:id 5 :children []} {:id 6 :children []}]}]}])
+;; Test 1: Einfacher 1..2-Baum (ohne Pflicht)
+(def module-1 {:type :module
+              :name "Modul 1"
+              :id "1"
+              :cp nil
+              :pordnr "1"
+              :mandatory false})
 
-(defn length_check "Check if count of sel is element of req"[sel req]
-  (some #(= (count sel) %) req))
+(def module-2 {:type :module
+              :name "Modul 2"
+              :id "2"
+              :cp nil
+              :pordnr "2"
+              :mandatory false})
 
-(declare check_children)
+(def level-1 {:type :level :min 1 :max 2 :name "Level 1"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [module-1 module-2]})
 
-(defn check_tree_node "Check if there are children for a single node" [tree_node]
-  (if (empty? (tree_node :children))
-    (println (tree_node :id))
-    (check_children (tree_node :children))))
+(def course-1 {:type :course
+             :degree "bk"
+             :kzfa "H"
+             :name "Major"
+             :po 2011
+             :cp nil
+             :course "maj"
+             :children [level-1]})
 
-(defn check_children [nodes]
-  (for [n nodes]
-    (check_tree_node n)))
+(deftest test-tree-1
+    (is (= (traverse-course course-1)
+      #{#{module-1} #{module-2} #{module-1 module-2}})))
 
-(defn ascending? [coll]
-  (every? (fn [[a b ]] (<= a b))
-    (partition 2 1 coll)))
+;; Test 2: Doppelter 1..2-Baum (ohne Pflicht)
+(def module-3 {:type :module
+              :name "Modul 3"
+              :id "3"
+              :cp nil
+              :pordnr "3"
+              :mandatory false})
 
-(def property
-  (prop/for-all [v (gen/vector gen/int)]
-    (let [s (sort v)]
-      (and (= (count v) (count s))
-        (ascending? s)))))
+(def module-4 {:type :module
+              :name "Modul 4"
+              :id "4"
+              :cp nil
+              :pordnr "4"
+              :mandatory false})
 
-(deftest quick-check
-  (do
-    newline
-    (println (tc/quick-check 100 property))))
+(def level-2 {:type :level :min 1 :max 2 :name "Level 2"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [module-3 module-4]})
+
+(def level-0 {:type :level :min 1 :max 2 :name "Level 0"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [level-1 level-2]})
+
+(def course-2 {:type :course
+             :degree "bk"
+             :kzfa "H"
+             :name "Major 2"
+             :po 2011
+             :cp nil
+             :course "maj"
+             :children [level-0]})
+
+(deftest test-tree-2
+    (is (= (traverse-course course-2)
+      #{#{module-1 module-3} #{module-1 module-4} #{module-2 module-3} #{module-2 module-4}})))
+
+;; Test 3: Doppelter 1..2-Baum mit nur drei verschieden Modulen
+(def level-3 {:type :level :min 1 :max 2 :name "Level 3"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [module-1 module-4]})
+
+(def level-4 {:type :level :min 2 :max 2 :name "Level 4"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [level-1 level-3]})
+
+(def course-3 {:type :course
+             :degree "bk"
+             :kzfa "H"
+             :name "Major 3"
+             :po 2011
+             :cp nil
+             :course "maj"
+             :children [level-4]})
+
+(deftest test-tree-3
+    (is (= (traverse-course course-3)
+      #{#{module-1 module-2} #{module-4 module-1} #{module-4 module-2}})))
+
+;; Test 4: Größerer Baum
+(def module-5 {:type :module
+              :name "Modul 5"
+              :id "5"
+              :cp nil
+              :pordnr "5"
+              :mandatory false})
+
+(def module-6 {:type :module
+              :name "Modul 6"
+              :id "6"
+              :cp nil
+              :pordnr "6"
+              :mandatory false})
+
+(def level-5 {:type :level :min 1 :max 3 :name "Level 5"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [module-1 module-2 module-3]})
+
+(def level-6 {:type :level :min 1 :max 2 :name "Level 6"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [module-4 module-5 module-6]})
+
+(def level-7 {:type :level :min 3 :max 3 :name "Level 7"
+            :min-cp nil
+            :max-cp nil
+            :tm nil
+            :art nil
+            :children [level-5 level-6]})
+
+(def course-4 {:type :course
+             :degree "bk"
+             :kzfa "H"
+             :name "Major 4"
+             :po 2011
+             :cp nil
+             :course "maj"
+             :children [level-7]})
+
+(deftest test-tree-4
+    (is (= (traverse-course course-4)
+      #{#{module-1 module-2 module-4} #{module-1 module-2 module-5} #{module-1 module-2 module-6}
+        #{module-1 module-3 module-4} #{module-1 module-3 module-5} #{module-1 module-3 module-6}
+        #{module-2 module-3 module-4} #{module-2 module-3 module-5} #{module-2 module-3 module-6}
+        #{module-1 module-4 module-5} #{module-1 module-4 module-6} #{module-1 module-5 module-6}
+        #{module-2 module-4 module-5} #{module-2 module-4 module-6} #{module-2 module-5 module-6}
+        #{module-3 module-4 module-5} #{module-3 module-4 module-6} #{module-3 module-5 module-6}})))
