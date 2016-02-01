12d6e803b56814d7b05e12f0bab91fb6a39030df
Philip Höfges <philip.hoefges@uni-duesseldorf.de>
2016-02-01 10:28:50 +0100

    Remove quick-check requirements

--- test/mincer/quick_check/combinations_test.clj
+++ test/mincer/quick_check/combinations_test.clj
@@ -1,9 +1,6 @@
 (ns mincer.quick_check.combinations_test
   (:require
     [clojure.test :refer :all]
-    [clojure.test.check :as tc]
-    [clojure.test.check.generators :as gen]
-    [clojure.test.check.properties :as prop]
     [mincer.module-combinations :refer :all]
     [mincer.xml.tree :refer :all]))
 
