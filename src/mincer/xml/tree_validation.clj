38adc0b2e72b02f9b705d0ec88f6b0a9199e2d90
David Schneider <david.schneider@bivab.de>
2016-03-14 14:43:18 +0100

    Validate course names

--- src/mincer/xml/tree_validation.clj
+++ src/mincer/xml/tree_validation.clj
@@ -8,7 +8,13 @@
 
 (defmethod validate :ModulBaum [mb]
   (log/trace (:tag mb))
-  (doall (flatten (map validate (:content mb)))))
+  ; validate pordnr
+  (doall (flatten (map validate (:content mb))))
+  ; validate course names
+  (let [course-names  (map #(-> % :attrs :name) (:content mb))]
+    (doseq [[name count] (freqs course-names)]
+      (log/error "Repeated course name in <ModuleData> section:" name "appears"
+                 count "times"))))
 
 (defmethod validate :b [b]
   (log/trace (:tag b))
