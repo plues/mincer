4ff205e80c9a81fc697984e3ad90c791db1eb4c3
David Schneider <david.schneider@bivab.de>
2016-07-05 15:18:05 +0200

    Updated project dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/java.jdbc "0.4.2"]
-                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.clojure/math.combinatorics "0.1.3"]
                  [org.clojure/test.check "0.9.0"]
-                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.14"]
+                 [org.slf4j/slf4j-log4j12 "1.7.21"]
                  [org.xerial/sqlite-jdbc "3.8.11.2"]
                  [seesaw "1.4.5"]]
   :plugins [[lein-launch4j "0.1.2"]]
