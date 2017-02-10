c80b9e112375aeefbcc4f058f797622e72e40614
David Schneider <david.schneider@bivab.de>
2017-02-10 13:58:06 +0100

    Fixed issue with swapped parameters, leading to courses and existing modules not being correctly associated

--- src/mincer/db.clj
+++ src/mincer/db.clj
@@ -189,7 +189,8 @@
 
 (defn course-module? [db-con course-id module-id]
   (let [records (jdbc/query db-con ["SELECT * FROM course_modules WHERE
-                                    module_id = ? AND course_id = ?" course-id module-id])]
+                                    course_id = ? AND module_id = ?" course-id module-id])]
+    (log/debug records)
     (pos? (count records)))) ; true if we have a link between course and module
 
 (defn load-course-module-map [db-con course-id]
