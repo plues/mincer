aee12124c603e9be525dadee14c6eea96922038e
David Schneider <david.schneider@bivab.de>
2017-06-08 09:12:34 +0200

    Bump version: 3.0.0 → 3.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mincer "3.0.0"
+(defproject mincer "3.1.0-SNAPSHOT"
   :description "Tool to create a SQLite database from PlüS module tree and data XML files."
   :url "https://github.com/plues/mincer"
   :license {:name "Eclipse Public License"
